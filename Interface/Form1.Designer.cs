55b69274d8cff82fc88b3b71cfd46ee1f8ed115c
Артем Симаков <artem.simakov.98@mail.ru>
2018-10-05 10:55:28 +0300

    v 1.0

--- Interface/Form1.Designer.cs
+++ Interface/Form1.Designer.cs
@@ -34,6 +34,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.portStatus = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // availablePorts
@@ -75,7 +79,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(666, 4);
+            this.label3.Location = new System.Drawing.Point(657, 4);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 13);
             this.label3.TabIndex = 4;
@@ -83,17 +87,62 @@
             // 
             // portStatus
             // 
-            this.portStatus.Location = new System.Drawing.Point(742, 4);
+            this.portStatus.ForeColor = System.Drawing.Color.Crimson;
+            this.portStatus.Location = new System.Drawing.Point(735, 4);
+            this.portStatus.Multiline = true;
             this.portStatus.Name = "portStatus";
-            this.portStatus.Size = new System.Drawing.Size(54, 20);
+            this.portStatus.Size = new System.Drawing.Size(61, 49);
             this.portStatus.TabIndex = 5;
             this.portStatus.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Выполнить команду";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(222, 86);
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(514, 223);
+            this.outputText.TabIndex = 8;
+            this.outputText.Text = "";
+            this.outputText.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(5, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Закрыть порт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(5, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Открыть порт";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.portStatus);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -102,6 +151,7 @@
             this.Controls.Add(this.availablePorts);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -116,6 +166,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox portStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox outputText;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Interface/Form1.cs
+++ Interface/Form1.cs
@@ -14,8 +14,8 @@ namespace Interface
     public partial class Form1 : Form
     {
         public Form1()
-        {   
-            
+        {
+
             InitializeComponent();
 
             availablePorts.Items.AddRange(JobsWithoutData.GetPortsName());
@@ -23,7 +23,7 @@ namespace Interface
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            availablePorts =  JobsWithoutData.SetPort();
+            JobsWithoutData.SetPort(availablePorts.Text);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -33,12 +33,70 @@ namespace Interface
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            portStatus.Text = JobsWithoutData.StatusPort();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            if (portStatus.Text == "Порт закрыт")
+            {
+                this.portStatus.ForeColor = System.Drawing.Color.OrangeRed;
+            }
+            else
+            {
+                this.portStatus.ForeColor = System.Drawing.Color.LightGreen;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (JobsWithoutData.StatusPort() == "Порт закрыт")
+            {
+                MessageBox.Show("Откройте порт");
+            }
+            else
+            {
+                byte[] am = { 0x1, 0x7 };
+
+                am = JobsWithoutData.Write(am);
+
+                for (int i = 0; i < am.Length; i++)
+                {
+                    outputText.Text += am[i].ToString("x");
+                    outputText.Text += " ";
+                }
+
+                //outputText.Text += Form.ToString("/n");
+
+                portStatus.Text = JobsWithoutData.StatusPort();
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            JobsWithoutData.ClosePort();
             portStatus.Text = JobsWithoutData.StatusPort();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            JobsWithoutData.OpenPort();
+            portStatus.Text = JobsWithoutData.StatusPort();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) => JobsWithoutData.ClosePort();
     }
 }
